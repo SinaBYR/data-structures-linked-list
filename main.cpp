4669f39748468b281bebb03635b0460f72a51c8a
SinaBYR <sina.beyraghdar1@gmail.com>
2021-12-22 16:46:46 +0330

    Add Linkedlist class

--- main.cpp
+++ main.cpp
@@ -7,6 +7,45 @@ class Node {
         Node *next;
 };
 
+class LinkedList {
+    private:
+        Node *head;
+        Node *current;
+        Node *temp;
+    public:
+        LinkedList(){
+            head = NULL;
+            current = NULL;
+            temp = NULL;
+        };
+
+        // ~LinkedList(){
+        //     Node *cutptr = head;
+        //     Node *temp;
+        //     while (cutptr != NULL){
+        //         temp = cutptr;
+        //         cutptr = cutptr->next;
+        //         delete temp;
+        //     }
+        // };
+
+        void addNode(int data) {
+            Node *n = new Node();
+            n->data = data;
+            n->next = NULL;
+
+            if(head) {
+                head = n;
+                return;
+            }
+
+            current = head;
+            while(current->next != NULL) {
+                current = current->next;
+            }
+            current->next = n;
+        };
+};
 
 int main(){
     return 0;
